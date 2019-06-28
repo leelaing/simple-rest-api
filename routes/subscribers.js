2a57093ed16023cc0996a5ed616f094dc4d89a28
leelaing <leelaing@liveloveyourlifenow.com>
2019-06-28 15:47:13 -0400

    Updated File names

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const Subscriber = require('../models/subscriber')
+const Subscriber = require('../models/subscribers')
 
 // Getting all
 router.get('/', async (req, res) => {
